73a3a861e34916b1cae41c707d365914b7554013
Doran Smestad <dsmestad@carbonblack.com>
2017-11-29 17:20:29 +0000

    series of minor changes to get testing to work and code cleanup

--- plugins/outputs/firehose/firehose_test.go
+++ plugins/outputs/firehose/firehose_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strconv"
 	"testing"
+	"github.com/stretchr/testify/assert"
 	//"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/testutil"
@@ -17,11 +18,14 @@ import (
 )
 
 // primary key spoof
-var recordID int = 0
+var recordID int64 = 0
 
 //req, resp := f.svc.PutRecordBatchRequest(batchInput)
 type mockFirehose struct {
 	firehoseiface.FirehoseAPI
+
+	// a link to the test interface to quickly fail tests
+	t *testing.T
 
 	// test values
 	expectedLines int64
@@ -39,20 +43,21 @@ type mockFirehose struct {
 // since errors (err) are eaten by the caller writeToFirehose
 func (m *mockFirehose) PutRecordBatch(input *firehose.PutRecordBatchInput) (output *firehose.PutRecordBatchOutput, err error) {
 	if len(input.Records) > 500 {
-		log.Printf("E! firehose_test: got more than 500 in a batch")
+		t.Log("E! firehose_test: got more than 500 in a batch")
+		m.t.Fail()
 	}
 	m.numOfPuts++
 
 	code := "42"
 	message := "Deliberate Error!"
 	errCount := m.numErrors
+
 	// insert errors
 	var entry firehose.PutRecordBatchResponseEntry
 	var responses []*firehose.PutRecordBatchResponseEntry
 
-
 	// create responses for output and process errors
-	for index, element := range input.Records {
+	for index, _ := range input.Records {   // TODO maybe something other than a range? range returns an int32
 		recordID++
 		if m.errorsRemain && int64(index) < errCount {
 			// inserting errors at first for specified error count
@@ -61,7 +66,7 @@ func (m *mockFirehose) PutRecordBatch(input *firehose.PutRecordBatchInput) (outp
 				ErrorMessage: &message }
 		} else {
 			m.errorsRemain = false
-			idString := strconv.Itoa(recordID)
+			idString := strconv.FormatInt(recordID, 10)
 			entry = firehose.PutRecordBatchResponseEntry{ RecordId: &idString }
 		}
 		responses = append(responses, &entry)
@@ -69,6 +74,7 @@ func (m *mockFirehose) PutRecordBatch(input *firehose.PutRecordBatchInput) (outp
 
 	batchOutput := firehose.PutRecordBatchOutput{ FailedPutCount: &errCount, RequestResponses: responses }
 
+	// TODO should this be at the top of the function to short-circuit other logic?
 	if errCount - (500 * (m.numOfPuts - 1)) >= 500 {
 		// simulated error, eaten by writeToFirehose (calling function)
 		err = errors.New("Total Failure Simulated")
@@ -78,57 +84,70 @@ func (m *mockFirehose) PutRecordBatch(input *firehose.PutRecordBatchInput) (outp
 }
 
 // TODO is err needed? check MockMetrics?
-func generateLines(numLines int) (lines []telegraf.Metric, err error) {
+func generateLines(numLines int64) (lines []telegraf.Metric, err error) {
 	err = nil
 	lines = testutil.MockMetrics()
 
 	// generate 1 less line then specified since
 	// the MockMetrics line returns a line when generated
-	for i := 0; i < (numLines-1); i++ {
+	for i := int64(0); i < (numLines-1); i++ {
 		lines = append(lines, testutil.TestMetric(1.0))
 	}
 	return
 }
 
 //
-func mockRun(f *FirehoseOutput, N int, E int64) (err error) {
-	if E > int64(N) {
-		return errors.New("More errors than records")
-	}
-	f.svc = mockFirehose{ numErrors: E, errorsRemain: true } // want bool(E)
+func mockRun(t *testing.T, f *FirehoseOutput, numMetrics int64, numErrors int64) (err error) {
+	f.svc = &mockFirehose{ numErrors: numErrors, errorsRemain: true, t: t} // want bool(E)
+
 	s, err := serializers.NewInfluxSerializer()
 	if err != nil {
+		t.Fail()
 		return
 	}
 	f.SetSerializer(s)
-	generatedLines, err := generateLines(N)
+	generatedLines, err := generateLines(numMetrics)
 	if err != nil {
+		t.Fail()
 		return
 	}
 	err = f.Write(generatedLines)
+
+	return
 }
 
-func checkBuffer(f *FirehoseOutput, E int64) (err error) {
-	count := len(f.errorBuffer)
-	if count != E {
-		err = errors.New(fmt.Sprintf("Got buffer length of %d, expected %d", count, E))
+// checkBuffer takes a quick look at how many firehose record errors were 
+// handled by the code.  It will error out if the number of errors found
+// are not equal to the number of errors expected.
+func checkBuffer(t *testing.T, f *FirehoseOutput, numErrors int64) (err error) {
+	count := int64(len(f.errorBuffer))
+	if count != numErrors {
+		err = errors.New(fmt.Sprintf("Got buffer length of %d, expected %d", count, numErrors))
+		t.Fail()
 	}
+	return
 }
 
-func testWrapper(t *testing.T, f *FirehoseOutput, N int, E int64) {
-	err = mockRun(f, N, E) // run one and total fail
+// testWrapper is a simple wrapper to add some test cases we always want to 
+// check when executing the code.
+func testWrapper(t *testing.T, f *FirehoseOutput, numMetrics int64, numErrors int64) {
+	if numErrors > numMetrics {
+		errors.New("More errors than records")
+		t.Fail()
+	}
+
+	err := mockRun(t, f, numMetrics, numErrors) // run one and total fail
 	if err != nil {
-		t.Fail(err)
+		t.Fail()
 	}
-	err = checkBuffer(f, E)
-	if err != nil {
-		t.Fail(err)
-	}
+
+	checkBuffer(t, f, numErrors)
 }
 
-// to be removed; reimplemented in suite function below
+// TestWriteToFirehoseAllSuccess tests the case when no error is returned 
+// from our mock AWS firehose function.
 func TestWriteToFirehoseAllSuccess(t *testing.T) {
-	f := FirehoseOutput{}
+	f := &FirehoseOutput{}
 	testWrapper(t, f, 10, 0)
 }
 
